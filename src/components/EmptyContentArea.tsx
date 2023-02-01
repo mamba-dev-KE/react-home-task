b291bdfa59c559c7108918419970c1949deb539e
Joseph Maramba <joseph@smartmile.eu>
2023-02-01 08:34:56 +0300

    styling table

--- src/components/EmptyContentArea.tsx
+++ src/components/EmptyContentArea.tsx
@@ -1,6 +1,6 @@
 const EmptyContentArea = () => {
   return (
-    <div className="content-area">
+    <div className="empty-content-area">
       <h2 className="no-items">What are you looking for?</h2>
       <p>Search for an item above</p>
     </div>
