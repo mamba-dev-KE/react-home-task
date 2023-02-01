54884b576ea0cd1fe1d2455a552cacda2f562e82
Joseph Maramba <joseph@smartmile.eu>
2023-02-01 15:53:50 +0300

    add useChecked hook

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,8 +1,7 @@
 import { useContext } from 'react';
-import { SearchContext } from '../context/Search';
-import { ToggleContext } from '../context/Toggle';
-import { ItemsContext } from '../context/Items';
+import { SearchContext, ToggleContext, ItemsContext, CheckedContext } from '../context';
 
 export const useSearch = () => useContext(SearchContext);
 export const useToggle = () => useContext(ToggleContext);
 export const useItems = () => useContext(ItemsContext);
+export const useChecked = () => useContext(CheckedContext);
