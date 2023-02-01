ea43bfcd428a8096fd92ebf6284780efbd0dc2fa
Joseph Maramba <joseph@smartmile.eu>
2023-02-01 09:01:54 +0300

    memoize filtered items

--- src/components/ContentArea.tsx
+++ src/components/ContentArea.tsx
@@ -1,36 +1,43 @@
+import { useMemo } from 'react';
 import { useItems, useSearch } from '../hooks';
+
+const TableHead = () => (
+  <thead>
+    <tr>
+      <th></th>
+      <th>Order #</th>
+      <th>Type</th>
+      <th>Item</th>
+    </tr>
+  </thead>
+);
 
 const ContentArea = () => {
   const { items } = useItems();
   const { search } = useSearch();
 
-  const filteredItems = items.filter(
-    (item) =>
-      item.type.toLowerCase().includes(search.toLowerCase()) ||
-      item.item_no.toString().includes(search.toLowerCase())
+  const filteredItems = useMemo(
+    () =>
+      items.filter(
+        (item) =>
+          item.type.toLowerCase().includes(search.toLowerCase()) ||
+          item.item_no.toString().includes(search.toLowerCase())
+      ),
+    []
   );
 
   return (
     <div className="content-area">
       <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Order #</th>
-            <th>Type</th>
-            <th>Item</th>
-          </tr>
-        </thead>
+        <TableHead />
         <tbody>
           {filteredItems.map(({ id, item_no, order_no, type }) => (
-            <>
-              <tr key={id}>
-                <td>{id}</td>
-                <td>{order_no}</td>
-                <td>{type}</td>
-                <td>{item_no}</td>
-              </tr>
-            </>
+            <tr key={id}>
+              <td>{id}</td>
+              <td>{order_no}</td>
+              <td>{type}</td>
+              <td>{item_no}</td>
+            </tr>
           ))}
         </tbody>
       </table>
